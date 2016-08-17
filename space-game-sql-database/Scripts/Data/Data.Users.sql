0c8cc021f97c91d60482d1d018e5798119d86566
Pete Jespers <petejespers@me.com>
2016-08-17 20:41:33 +0100

    SG-129 Switch to SQL Server 2014

---
+++ space-game-sql-database/Scripts/Data/Data.Users.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Users] ON 
+
+GO
+INSERT [dbo].[Users] ([Id], [Name], [CreatedBy], [CreatedDateUtc], [ModifiedBy], [ModifiedDateUtc]) VALUES (1, N'Paul Jespers', 1, CAST(N'2016-07-06 20:05:55.8700000' AS DateTime2), 1, CAST(N'2016-07-06 20:05:55.8700000' AS DateTime2))
+GO
+SET IDENTITY_INSERT [dbo].[Users] OFF
+GO
