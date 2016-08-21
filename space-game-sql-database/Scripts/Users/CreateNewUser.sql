f8753caf0e67248a6f98b9454cd1dbe283be7329
Pete Jespers <petejespers@me.com>
2016-08-21 08:46:27 +0100

    Basic script for creating a new user

---
+++ space-game-sql-database/Scripts/Users/CreateNewUser.sql
@@ -0,0 +1,5 @@
+﻿CREATE LOGIN [IIS APPPOOL\MyAppPoolName] FROM WINDOWS;
+--USE [MydatabaseName];
+CREATE USER [External-SpaceGame] FOR LOGIN [IIS APPPOOL\MyAppPoolName];
+
+--TODO: Add permissions to user SG-133
